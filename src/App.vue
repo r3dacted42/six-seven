8538829e5ad1028e8dacb1b09deb25b9f2e874da
priyansh agrahari <75441026+r3dacted42@users.noreply.github.com>
2025-11-02 05:18:34 +0530

    add workflow

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
 import { useParallax } from '@vueuse/core';
 import { computed, type CSSProperties, useTemplateRef } from 'vue';
 
-const container = useTemplateRef("container");
-const { tilt, roll } = useParallax(container);
-const factor = 20;
+const container = useTemplateRef("scp");
+const { tilt } = useParallax(container);
+const factor = 30;
 
 const leftHandStyle = computed<CSSProperties>(() => ({
     transform: `translateY(${tilt.value * factor}%)`,
@@ -15,47 +15,26 @@ const rightHandStyle = computed<CSSProperties>(() => ({
 </script>
 
 <template>
-    <div ref="container" class="container">
-        <div class="scp">
-            <img :style="leftHandStyle" class="left" src="/hand_left.png" />
-            <img :style="rightHandStyle" class="right" src="/hand_right.png" />
-        </div>
+    <div ref="scp" class="scp">
+        <img :style="leftHandStyle" style="left: 0;" src="/hand_left.png" />
+        <img :style="rightHandStyle" style="right: 0;" src="/hand_right.png" />
     </div>
 </template>
 
 <style scoped>
-.container {
-    height: 100%;
-    display: flex;
-    place-content: center;
-    --hand-top: 50%;
-
-    * {
-        min-width: 0;
-    }
-}
-
 .scp {
+    height: 100%;
     width: 100%;
-    height: auto;
-    aspect-ratio: 16 / 9;
     position: relative;
     background-image: url(/scp.png);
     background-repeat: no-repeat;
     background-position: center;
     background-size: contain;
-}
 
-.left {
-    position: absolute;
-    left: 0;
-    top: var(--hand-top);
-    width: 45%;
-}
-.right {
-    position: absolute;
-    right: 0;
-    top: var(--hand-top);
-    width: 45%;
+    img {
+        position: absolute;
+        top: 50%;
+        width: 45%;
+    }
 }
 </style>
